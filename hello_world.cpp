024f7013367b33c07391bc0dc3ed8ba4f7852123
Makhdi1 <makhdisaf@gmail.com>
2026-03-14 08:49:14 +0300

    Apply clang-format with Mozilla style Oy Yeee

--- hello_world.cpp
+++ hello_world.cpp
@@ -7,4 +7,12 @@ int main() {
     cin >> name;
     cout << "Hello my friend " << name << endl;
     return 0;
+int
+main()
+{
+  string name;
+  cout << "Enter ur name: ";
+  cin >> name;
+  cout << "Hello " << name << endl;
+  return 0;
 }
